0cd9f25d0617a21577384f00fd402f40ed583aaa
Nicholas Andrew Wandinata <wrx481@hydra0.eecs.utk.edu>
2023-04-15 16:14:21 -0400

    makefile

--- proj5/worddice.cpp
+++ proj5/worddice.cpp
@@ -156,7 +156,7 @@ int main(int argc, char *argv[]) {
 
 	//while(true) {
 		finW >> word;
-		graph->add_word_to_graph(word, id)
+		graph->add_word_to_graph(word, id);
 		graph->dump_nodes();
 		// Delete word
 		id += 1;
